eb41d35f0efc37125d9791ab2c94c22f4eaae8bb
churakovra <churakov.main@gmail.com>
2025-04-15 20:05:23 +0300

    add logic for bot statring

--- app/main.py
+++ app/main.py
@@ -1,46 +1,20 @@
 import asyncio
 import logging
-import sys
 
-from aiogram import Dispatcher, Bot, F
-from aiogram.filters import Command
-from aiogram.types import Message
-from app.config.schedule_config import TOKEN
-from app.models import model
-from app.models.model import *
-from app.utils.strings import Strings as s
+from aiogram import Bot, Dispatcher
 
-dp = Dispatcher()
+from app.config.preferences import BOT_TOKEN
 
 
-@dp.message(Command('set_lesson'))
-async def set_lesson(message: Message):
-    message_text = s.GREETING
-    branch_markup = markups.get_set_types_markup()
-    await message.answer(text=message_text, reply_markup=branch_markup)
+async def main():
+    logging.basicConfig(level=logging.INFO)
 
+    bot = Bot(BOT_TOKEN)
+    dp = Dispatcher()
 
-@dp.callback_query(F.data.startswith('branch_'))
-async def get_branch_handler(callback: CallbackQuery):
-    match callback.data:
-        case s.CALLBACK_BRANCH_DAYS:
-            await model.send_select_day_message(callback)
-        case s.CALLBACK_BRANCH_MANUAL:
-            await model.send_set_manual_message(callback)
-
-
-@dp.callback_query(F.data.startswith('day_'))
-async def get_day_schedule_handler(callback: CallbackQuery):
-    await model.send_day_schedule(callback)
-
-
-async def main() -> None:
-    bot = Bot(token=TOKEN)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-print("Another test")
