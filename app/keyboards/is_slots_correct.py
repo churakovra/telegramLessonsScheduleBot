8496fd5bb8f0ece28473347385346126500ecf4d
churakovra <churakov.main@gmail.com>
2025-04-30 00:04:36 +0300

    replaced hardcoded text with value from utils/bot_strings

--- app/keyboards/is_slots_correct.py
+++ app/keyboards/is_slots_correct.py
@@ -1,17 +1,19 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from app.utils.bot_strings import bot_strings as bt
+
 
 def get_is_slots_correct_markup() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.add(
         InlineKeyboardButton(
-            text="Да",
-            callback_data="slots_correct"
+            text=bt.YES,
+            callback_data=bt.CALLBACK_SLOTS_CORRECT
         ),
         InlineKeyboardButton(
-            text="Нет",
-            callback_data="slots_incorrect"
+            text=bt.NO,
+            callback_data=bt.CALLBACK_SLOTS_INCORRECT
         )
     )
     return builder.as_markup()
