a0b2225fff104f9668de4da9b4c0882ee14638be
churakovra <churakov.main@gmail.com>
2025-04-30 00:03:08 +0300

    add slots incorrect callback handler

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,6 +2,8 @@ from aiogram import Dispatcher
 
 from app.handlers.commands.new_schedule import router as new_schedule
 from app.handlers.states.wait_for_slots import router as wait_for_slots
+from app.handlers.callbacks.slots_correct import router as slots_correct
+from app.handlers.callbacks.slots_incorrect import router as slots_incorrect
 from app.handlers.callbacks.new_lesson_day_callback import router as new_lesson_callback_router
 from app.handlers.commands.new_lesson import router as new_lesson_router
 from app.handlers.commands.start import router as start_router
@@ -9,6 +11,8 @@ from app.handlers.commands.start import router as start_router
 routers = [
     start_router,
     wait_for_slots,
+    slots_correct,
+    slots_incorrect,
     new_schedule,
     new_lesson_router,
     new_lesson_callback_router
---
+++ app/handlers/callbacks/slots_incorrect.py
@@ -0,0 +1,17 @@
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
+from app.states.schedule_states import ScheduleStates
+from app.utils.bot_strings import bot_strings as bt
+
+router = Router()
+
+
+@router.callback_query(F.data == bt.CALLBACK_SLOTS_INCORRECT)
+async def retry_send_slots(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        text=bt.SLOTS_FAILURE_ANSWER
+    )
+    await state.set_state(ScheduleStates.wait_for_slots)
+    await callback.answer()
