ffc68008d9bab4bd84907ed7bceffa0ab06a743e
churakovra <churakov.main@gmail.com>
2025-04-30 00:04:53 +0300

    replaced hardcoded text with value from utils/bot_strings

--- app/handlers/states/wait_for_slots.py
+++ app/handlers/states/wait_for_slots.py
@@ -4,6 +4,8 @@ from aiogram.types import Message
 
 from app.keyboards.is_slots_correct import get_is_slots_correct_markup
 from app.states.schedule_states import ScheduleStates
+from app.utils.bot_strings import bot_strings as bt
+from app.utils.create_parsed_slots_message_text import create_parsed_slots_message_text
 from app.utils.parse_slots import parse_slots
 
 router = Router()
@@ -12,13 +14,9 @@ router = Router()
 @router.message(ScheduleStates.wait_for_slots)
 async def wait_for_slots(message: Message, state: FSMContext):
     slots_raw = message.text
-    await message.answer("Получил, обрабатываю")
-    parsed_lessons = parse_slots(slots_raw, message.from_user.username)
-    await message.answer("Обработал!")
-    slot_message = ""
-    for slot in parsed_lessons:
-        slot_message += (f"📅: {slot.day_name}, {slot.slot_date}\n"
-                         f"🕐: {", ".join(map(lambda slot_time: slot_time.strftime("%H:%M"), slot.available_time))}\n\n")
-    slot_message += "Верно?"
+    await message.answer(bt.PARSING_SLOTS_PROCESSING)
+    parsed_lessons = await parse_slots(slots_raw, message.from_user.username)
+    await message.answer(bt.PARSING_SLOTS_SUCCESS)
+    slot_message = create_parsed_slots_message_text(parsed_lessons)
     await message.answer(slot_message, reply_markup=get_is_slots_correct_markup())
     await state.clear()
