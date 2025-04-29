6235d06e24d2073292d0dcb769d5f74f032ce14f
churakovra <churakov.main@gmail.com>
2025-04-29 22:42:56 +0300

    update wait fot slots state def

--- app/handlers/states/wait_for_slots.py
+++ app/handlers/states/wait_for_slots.py
@@ -1,10 +1,9 @@
-import asyncio
-
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from app.states.schedule_states import ScheduleStates
+from app.utils.parse_slots import parse_slots
 
 router = Router()
 
@@ -12,8 +11,7 @@ router = Router()
 @router.message(ScheduleStates.wait_for_slots)
 async def wait_for_slots(message: Message, state: FSMContext):
     slots_raw = message.text
-    print(slots_raw)
     await message.answer("Получил, обрабатываю")
-    await asyncio.sleep(3)
+    parsed_lessons = parse_slots(slots_raw, message.from_user.username)
     await message.answer("Обработал!")
     await state.clear()
