1f4625452009e2fb54d3dca136d527fd9f7dda38
churakovra <churakov.main@gmail.com>
2025-05-01 23:45:18 +0300

    remove non-used models

--- app/models/gap.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass
-class Gap:
-    number: int #может пригодится в будущем нумеровка урока
-    datetime_start: datetime
-    datetime_end: datetime
--- app/models/lesson.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-class Lesson:
-    def __init__(self, id_teacher: int, id_student: int, id_lesson_type: int, datetime_start: datetime):
-        self.id_teacher = id_teacher
-        self.id_student = id_student
-        self.id_lesson_type = id_lesson_type
-        self.datetime_start = datetime_start
