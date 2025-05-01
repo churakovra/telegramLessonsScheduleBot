ff8dff9ff84244712695b9e8475534c731740fe6
churakovra <churakov.main@gmail.com>
2025-05-01 20:20:57 +0300

    add database init

--- app/config/preferences.py
+++ app/config/preferences.py
@@ -11,4 +11,3 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-DB_PATH = f"postgresql://${DB_USER}:${DB_PASSWORD}@{DB_HOST}:{DB_PORT}/${DB_NAME}"
