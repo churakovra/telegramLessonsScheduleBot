b52d69b8879391c597b95ba369505d68230e851a
churakovra <churakov.main@gmail.com>
2025-05-02 15:53:10 +0300

    declare "lesson_types" table

---
+++ app/models/lesson_type.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+
+class LessonType(Base):
+    __tablename__ = "lesson_types"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    duration: Mapped[float]
+
+    lessons: Mapped[List["Lesson"]] = relationship(back_populates="")
