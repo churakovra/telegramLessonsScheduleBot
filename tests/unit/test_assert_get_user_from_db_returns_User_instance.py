ab0a2d80508be9ad073c3049f9aa42f0d4dcd15c
churakovra <churakov.main@gmail.com>
2025-05-07 01:11:56 +0300

    add unit test for get user from db def

---
+++ tests/unit/test_assert_get_user_from_db_returns_User_instance.py
@@ -0,0 +1,7 @@
+from app.models.user import User
+from app.services.get_user_from_db import get_user
+
+
+async def test_assert_get_user_from_db_returns_User_instance():
+    user = await get_user("johndoe")
+    assert isinstance(user, User) == True
