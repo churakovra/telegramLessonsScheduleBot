ff5b9960f93a238d9c6632fd6a317e3f45ae004a
churakovra <churakov.main@gmail.com>
2025-05-06 19:48:19 +0300

    add getting user from db def

---
+++ app/services/get_user_info_from_db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+from app.models.student import Student
+from app.models.teacher import Teacher
+from app.models.admin import Admin
+from app.models.user import User
+from app.database import SessionLocal
+from app.utils.make_user_info_response import make_user_info_response
+
+
+async def get_user_info(username: str) -> str:
+    user_stmt = select(User).where(User.username==username)
+    with SessionLocal.begin() as session:
+        user = session.scalar(user_stmt)
+    if not user:
+        return "Такого не нашли"
+    result = make_user_info_response(user)
+    return result
