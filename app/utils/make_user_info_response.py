72f7b37a02dc996e6f57e0aee7862fc5bf18264d
churakovra <churakov.main@gmail.com>
2025-05-07 13:07:05 +0300

    make make_user_info_response

--- app/utils/make_user_info_response.py
+++ app/utils/make_user_info_response.py
@@ -1,17 +1,20 @@
 from app.models.user import User
+from app.models.user_dto import UserDTO
+
+from app.utils.datetime_utils import day_format
+from app.utils.bot_strings import bot_strings as bt
+from app.utils.bot_values import BotValues as bv
 
 
-def make_user_info_response(user: User) -> str:
-    role = ""
-    match user:
-        case _ if user.teacher:
-            role = "Преподаватель"
-        case _ if user.student:
-            role = "Студент"
-        case _ if user.admin:
-            role = "Администратор"
-    return f"""
-        Пользователь {user.username}
-        Дата регистрации {user.dt_reg}
-        Роль {role}
-    """
+def make_user_info_response(user: UserDTO, user_status: bv.UserRoles) -> str:
+    try:
+        result = (
+            f"Пользователь {user.firstname} {user.lastname}\n"
+            f"Имя пользователя {user.username}\n"
+            f"Статус {user_status.value}\n"
+            f"Дата регистрации {user.dt_reg.strftime(day_format)}\n"
+        )
+    except Exception:
+        result = bt.USER_INFO_ERROR
+
+    return result
