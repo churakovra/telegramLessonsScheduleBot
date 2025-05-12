cd6116dd38269a9d9b55092fce2e29aea6b3c60a
churakovra <churakov.main@gmail.com>
2025-05-12 14:52:47 +0300

    new_schedule refactor remove parse_slots and make slots logic from project

--- app/handlers/commands/__init__.py
+++ app/handlers/commands/__init__.py
@@ -1,7 +1,6 @@
 from .start import router as start_router
 from .my_info import router as info_router
 from .new_schedule import router as new_schedule_router
-from .make_slots import router as make_slots_router
 from .slots_error import router as slots_error_router
 from .make_teacher import router as make_teacher_router
 
@@ -9,7 +8,6 @@ command_routers = [
     start_router,
     info_router,
     new_schedule_router,
-    make_slots_router,
     slots_error_router,
     make_teacher_router
 ]
