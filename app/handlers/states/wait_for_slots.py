e8fc8594f738b030ef8de1acd2d7e78493406ee1
churakovra <churakov.main@gmail.com>
2025-05-12 14:51:40 +0300

    new_schedule refactor edit wait_for_slots state, add usecase logic into it

--- app/handlers/states/wait_for_slots.py
+++ app/handlers/states/wait_for_slots.py
@@ -2,12 +2,25 @@ from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from app.handlers.commands.make_slots import make_slots
+from app.keyboards.is_slots_correct import get_is_slots_correct_markup
 from app.states.schedule_states import ScheduleStates
+from app.use_cases.set_new_schedule import set_new_schedule_use_case
+from app.utils.create_parsed_slots_message_text import create_parsed_slots_message_text
 
 router = Router()
 
 
 @router.message(ScheduleStates.wait_for_slots)
 async def wait_for_slots(message: Message, state: FSMContext):
-    await make_slots(message, state)
+    slots_raw = message.text
+    message_from = message.from_user.username
+    slots = await set_new_schedule_use_case(slots_raw, message_from)
+
+    slots_reply = create_parsed_slots_message_text(slots)
+    await message.answer(
+        text=slots_reply,
+        reply_markup=get_is_slots_correct_markup()
+    )
+
+    await state.update_data(slots=slots)
+    await state.set_state(ScheduleStates.wait_for_confirmation)
