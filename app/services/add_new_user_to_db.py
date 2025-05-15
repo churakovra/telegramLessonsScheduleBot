34180f2b2204e0688419e0522488360bcd9b71a9
churakovra <churakov.main@gmail.com>
2025-05-15 10:03:37 +0300

    make user_repo

--- app/services/add_new_user_to_db.py
+++
@@ -1,9 +0,0 @@
-from app.models.orm.student import Student
-from app.models.orm.user import User
-from app.database import SessionLocal
-
-
-async def add_user(user: User):
-    with SessionLocal.begin() as session:
-        session.add(user)
-        session.add(Student(user=user))
