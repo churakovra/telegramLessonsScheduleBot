0f5d06e2797200a957563fd83d6a67de343319f3
churakovra <churakov.main@gmail.com>
2025-05-16 20:55:15 +0300

    make user_exceptions

---
+++ app/exceptions/user_exceptions.py
@@ -0,0 +1,12 @@
+class UserStatusError(Exception):
+    def __init__(self, message: str = "Недостаточно прав у пользователя"):
+        super().__init__(message)
+
+class AddUserError(Exception):
+    def __init__(self, message: str = "Ошибка добавления пользователя. Пользователь уже существует."):
+        super().__init__(message)
+
+
+class ChangeUserStatusError(Exception):
+    def __init__(self, message: str = "Ошибка изменения статуса пользователя."):
+        super().__init__(message)
