35d120ef720c91d16b1faaa3a0c6fe39bc22f745
churakovra <churakov.main@gmail.com>
2025-05-16 21:02:34 +0300

    remove info_router from __init__ in commands package

--- app/handlers/commands/__init__.py
+++ app/handlers/commands/__init__.py
@@ -1,11 +1,9 @@
 from .start import router as start_router
-from .my_info import router as info_router
 from .new_slots import router as new_schedule_router
 from .make_teacher import router as make_teacher_router
 
 command_routers = [
     start_router,
-    info_router,
     new_schedule_router,
     make_teacher_router
 ]
