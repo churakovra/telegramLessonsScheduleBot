41b5fd22ff71f3d93c95710b11de28d2c76e6f97
churakovra <churakov.main@gmail.com>
2025-05-19 15:14:20 +0300

    add get_students def

--- app/repositories/teacher_repo.py
+++ app/repositories/teacher_repo.py
@@ -1,5 +1,15 @@
+from sqlalchemy import select
+
 from app.database import SessionLocal
 from app.models.orm.teacher_students import TeacherStudents
+from app.models.orm.teacher import Teacher
+from app.models.orm.student import Student
+from app.models.orm.admin import Admin
+from app.models.orm.user import User
+
+from app.models.orm.lesson_type import LessonType
+from app.models.orm.lesson import Lesson
+
 from app.models.user_dto import UserDTO
 
 
@@ -13,3 +23,18 @@ class TeacherRepo:
         except Exception:
             return False
         return True
+
+    @staticmethod
+    async def get_students(teacher: str) -> list[UserDTO]:
+        users = list()
+        stmt = (
+            select(User)
+            .join(TeacherStudents, User.username == TeacherStudents.student_username)
+            .where(TeacherStudents.teacher_username == teacher)
+        )
+        with SessionLocal.begin() as session:
+            res = session.execute(stmt)
+            for index, row in enumerate(res):
+                user = UserDTO.get_user_dto(row[index])
+                users.append(user)
+        return users
--- app/services/teacher_service.py
+++ app/services/teacher_service.py
@@ -1,3 +1,5 @@
+from app.models.user_dto import UserDTO
+from app.repositories.teacher_repo import TeacherRepo
 from app.services.user_service import UserService
 from app.utils.bot_values import BotValues
 
@@ -6,8 +8,8 @@ roles = BotValues.UserRoles
 
 class TeacherService(UserService):
     @staticmethod
-    async def get_teachers_students():
-        pass
+    async def get_students(teacher_username: str) -> list[UserDTO]:
+        return await TeacherRepo.get_students(teacher_username)
 
     @staticmethod
     async def check_user_status(username: str, expected_status: roles = roles.TEACHER) -> bool:
