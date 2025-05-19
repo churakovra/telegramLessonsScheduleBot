6194874afce563b8a66da0fb36104f1e7b2bcaf4
churakovra <churakov.main@gmail.com>
2025-05-19 15:14:45 +0300

    add send_message at telegram_notifier.py

--- app/notifiers/telegram_notifier.py
+++ app/notifiers/telegram_notifier.py
@@ -1,5 +1,12 @@
 from aiogram import Bot
 
+
 class TelegramNotifier:
     def __init__(self, bot: Bot):
         self.bot = bot
+
+    async def send_message(self, chat_id: int, message: str):
+        await self.bot.send_message(
+            chat_id=chat_id,
+            text=message
+        )
