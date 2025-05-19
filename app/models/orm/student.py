ba2f37fadfe1925fe039e8b8c9bd7ab4d4311226
churakovra <churakov.main@gmail.com>
2025-05-19 10:22:17 +0300

    add teacher_students model

--- app/models/orm/student.py
+++ app/models/orm/student.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.orm.base import Base
 from app.models.orm.lesson import Lesson
+from app.models.orm.teacher_students import TeacherStudents
 from app.models.orm.user import User
 
 
@@ -10,8 +11,9 @@ class Student(Base):
     __tablename__ = "students"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    username: Mapped[str] = mapped_column(String, ForeignKey("users.username"), nullable=False)
+    username: Mapped[str] = mapped_column(String, ForeignKey("users.username"), nullable=False, unique=True)
     notifications: Mapped[bool] = mapped_column(default=True)
 
     user: Mapped["User"] = relationship(back_populates="student")
     lessons: Mapped[list["Lesson"]] = relationship(back_populates="student")
+    teacher_students: Mapped["TeacherStudents"] = relationship(back_populates="student")
