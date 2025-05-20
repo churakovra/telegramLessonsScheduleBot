925c0915d37323d482c7dab5ef8f5fb1c353cf79
churakovra <churakov.main@gmail.com>
2025-05-20 22:04:57 +0300

    add reply markup at telegram_notifier.py

--- app/notifiers/telegram_notifier.py
+++ app/notifiers/telegram_notifier.py
@@ -1,12 +1,14 @@
 from aiogram import Bot
+from aiogram.types import InlineKeyboardMarkup
 
 
 class TelegramNotifier:
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    async def send_message(self, chat_id: int, message: str):
+    async def send_message(self, chat_id: int, message: str, reply_markup: InlineKeyboardMarkup | None = None):
         await self.bot.send_message(
             chat_id=chat_id,
-            text=message
+            text=message,
+            reply_markup=reply_markup
         )
