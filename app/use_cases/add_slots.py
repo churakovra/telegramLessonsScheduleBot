9c2dc9b010671d529926e6ebf139078643c5d550
churakovra <churakov.main@gmail.com>
2025-05-21 22:55:44 +0300

    add add_slots_use_case

--- app/use_cases/add_slots.py
+++ app/use_cases/add_slots.py
@@ -1,6 +1,8 @@
+from uuid import UUID
+
 from app.models.teacher_slot import Slot
 from app.repositories.slots_repo import SlotsRepo
 
 
-async def add_slots_use_case(slots: list[Slot]):
-    await SlotsRepo.add_slots(slots)
+async def add_slots_use_case(slots: list[Slot]) -> dict[str, UUID]:
+    return await SlotsRepo.add_slots(slots)
