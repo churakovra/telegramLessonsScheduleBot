7e8cb7b8455518b7989ffeec18debc55be97f0e3
churakovra <churakov.main@gmail.com>
2025-05-22 18:13:55 +0300

    add send slots logic at send slots to students use case

--- app/use_cases/send_slots_to_students.py
+++ app/use_cases/send_slots_to_students.py
@@ -1,9 +1,20 @@
-from app.models.teacher_slot import Slot
+from uuid import UUID
+
+from app.keyboards.days_for_students_markup import get_days_for_students_markup
 from app.notifiers.telegram_notifier import TelegramNotifier
 from app.services.teacher_service import TeacherService
 
 
-async def send_slots_to_students_use_case(slots: list[Slot], notifier: TelegramNotifier):
-    teacher = slots[0].teacher
+async def send_slots_to_students_use_case(
+        teacher: str,
+        slots: dict[str, UUID],
+        notifier: TelegramNotifier
+):
     students = await TeacherService.get_students(teacher)
-    pass
+    markup = get_days_for_students_markup(slots)
+    for student in students:
+        await notifier.send_message(
+            student.chat_id,
+            f"Сообщение от нотифаера, {teacher} - учитель",
+            reply_markup=markup
+        )
