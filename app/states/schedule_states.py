cf7da0838274597baafc642a87233c4eae0a442e
churakovra <churakov.main@gmail.com>
2025-05-22 18:15:11 +0300

    remove unused state

--- app/states/schedule_states.py
+++ app/states/schedule_states.py
@@ -5,4 +5,3 @@ class ScheduleStates(StatesGroup):
     wait_for_teacher_username = State()
     wait_for_slots = State()
     wait_for_confirmation = State()
-    wait_slots_send_to_students = State()
