a65e4609756a57bdf76ffef05ca7dfd7f091ed04
churakovra <churakov.main@gmail.com>
2025-05-23 17:47:28 +0300

    rename routers in callback init

--- app/handlers/callbacks/__init__.py
+++ app/handlers/callbacks/__init__.py
@@ -1,11 +1,13 @@
 from .slots_correct import router as slots_correct_router
 from .slots_incorrect import router as slots_incorrect_router
-from .days_for_students import router as days_for_students_router
+from .day_button_handle import router as day_button_handle_router
+from .slot_button_handle import router as slot_button_handle_router
 from .user_info import router as user_info_router
 
 callback_routers = [
     slots_correct_router,
     slots_incorrect_router,
-    days_for_students_router,
+    day_button_handle_router,
+    slot_button_handle_router,
     user_info_router
 ]
