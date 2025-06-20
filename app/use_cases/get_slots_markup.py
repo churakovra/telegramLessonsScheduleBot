f6ad445cb501e0da25d40671fad0916725a6b37c
churakovra <churakov.main@gmail.com>
2025-06-20 19:54:53 +0300

    feat(bot-0.0.0): delete all use cases

--- app/use_cases/get_slots_markup.py
+++
@@ -1,9 +0,0 @@
-from uuid import UUID
-
-from app.keyboards.slots_for_students_markup import get_slots_for_students_markup
-from app.repositories.slots_repo import SlotsRepo
-
-
-async def get_slots_markup_use_case(uuid_day: UUID, chat_id: int):
-    slots = await SlotsRepo.get_slots(uuid_day)
-    return get_slots_for_students_markup(slots, chat_id)
