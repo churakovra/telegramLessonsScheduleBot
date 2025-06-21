dae69f2d884f4744716a19df04988eba7a36c4f7
churakovra <churakov.main@gmail.com>
2025-06-21 21:13:46 +0300

    feat(bot-0.0.0): rm attach to teacher logic from /start handler

--- app/handlers/commands/start.py
+++ app/handlers/commands/start.py
@@ -1,19 +1,17 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from sqlalchemy.orm import Session
 
-from app.handlers.commands.new_slots import UserRoles
+from app.enums.bot_values import UserRoles
 from app.services.user_service import UserService
-from app.states.schedule_states import ScheduleStates
 from app.utils.bot_strings import bot_strings as bt
 
 router = Router()
 
 
 @router.message(Command("start"))
-async def add_new_user(message: Message, state: FSMContext, session: Session):
+async def add_new_user(message: Message, session: Session):
     user_service = UserService(session)
     username = message.from_user.username
     firstname = message.from_user.first_name
@@ -26,7 +24,4 @@ async def add_new_user(message: Message, state: FSMContext, session: Session):
         role=UserRoles.STUDENT,
         chat_id=chat_id
     )
-    await message.answer(
-        text=bt.GREETING.format(firstname, username)
-    )
-    await state.set_state(ScheduleStates.wait_for_teacher_username)
+    await message.answer(text=bt.GREETING.format(firstname))
