9f4f8457c3f1915683657f0bc81c7b14e84c745c
churakovra <churakov.main@gmail.com>
2025-06-27 23:20:27 +0300

    feat(bot-0.0.0): remove unused back pressed logic

--- app/handlers/callbacks/__init__.py
+++ app/handlers/callbacks/__init__.py
@@ -2,7 +2,6 @@ from .slots_correct import router as slots_correct_router
 from .slots_incorrect import router as slots_incorrect_router
 from .day_button_handle import router as day_button_handle_router
 from .slot_button_handle import router as slot_button_handle_router
-from .back_button_handle import router as back_button_handle_router
 from .user_info import router as user_info_router
 
 callback_routers = [
@@ -10,6 +9,5 @@ callback_routers = [
     slots_incorrect_router,
     day_button_handle_router,
     slot_button_handle_router,
-    back_button_handle_router,
     user_info_router
 ]
