dce739f67853d58c5db1af54db141ec9be23e80c
churakovra <churakov.main@gmail.com>
2025-07-14 22:30:41 +0300

    feature(bot-0.0.0): update __init__ callbacks & commands

--- app/handlers/callbacks/__init__.py
+++ app/handlers/callbacks/__init__.py
@@ -1,13 +1,15 @@
+from .day_button_handle import router as day_button_handle_router
+from .menu_teacher_cg import router as menu_teacher_cg_router
+from .slot_button_handle import router as slot_button_handle_router
 from .slots_correct import router as slots_correct_router
 from .slots_incorrect import router as slots_incorrect_router
-from .day_button_handle import router as day_button_handle_router
-from .slot_button_handle import router as slot_button_handle_router
 from .user_info import router as user_info_router
 
 callback_routers = [
+    day_button_handle_router,
+    menu_teacher_cg_router,
+    slot_button_handle_router,
     slots_correct_router,
     slots_incorrect_router,
-    day_button_handle_router,
-    slot_button_handle_router,
     user_info_router
 ]
