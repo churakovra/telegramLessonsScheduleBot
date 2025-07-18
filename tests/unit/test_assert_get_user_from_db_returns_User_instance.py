f0bcde620ee8edae4f504c0c7f055d242138c6ab
churakovra <churakov.main@gmail.com>
2025-07-18 22:50:22 +0300

    wip: prepare for tests

--- tests/unit/test_assert_get_user_from_db_returns_User_instance.py
+++
@@ -1,7 +0,0 @@
-from app.db.orm import User
-from app.services.get_user_from_db import get_user
-
-
-async def test_assert_get_user_from_db_returns_User_instance():
-    user = await get_user("johndoe")
-    assert isinstance(user, User) == True
