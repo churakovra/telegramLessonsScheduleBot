d4abea5bcb75066e832c4e1a24aee5c7aae13d97
churakovra <churakov.main@gmail.com>
2025-07-23 23:46:38 +0300

    feature(students): add teacher students show logic; update exceptions: add self.fields for better exception handling

--- app/handlers/callbacks/teacher/__init__.py
+++ app/handlers/callbacks/teacher/__init__.py
@@ -5,9 +5,11 @@ from .slots_correct import router as slots_correct_router
 from .teacher_lesson_add import router as teacher_lesson_add_router
 from .teacher_slot_add import router as teacher_slot_add_router
 from .teacher_student_add import router as teacher_student_add_router
+from .teacher_student_list import router as teacher_student_list_router
 
 teacher_routers = [
     teacher_lesson_add_router,
     teacher_slot_add_router,
     teacher_student_add_router,
+    teacher_student_list_router,
 ]
