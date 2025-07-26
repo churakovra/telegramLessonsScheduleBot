0fc39dfe48837c3204d6226c84f35d4c42021571
churakovra <churakov.main@gmail.com>
2025-07-26 22:50:35 +0300

    feature(lessons): create teacher/lesson & teacher/student callback packages

--- app/handlers/callbacks/teacher/__init__.py
+++ app/handlers/callbacks/teacher/__init__.py
@@ -1,17 +1,15 @@
+from .lesson import lesson_routers
 from .send_slots import router as send_slots_router
 
 from .slots_correct import router as slots_correct_router
+from .student import student_routers
 
-from .teacher_lesson_add import router as teacher_lesson_add_router
+
 from .teacher_slot_add import router as teacher_slot_add_router
-from .teacher_student_add import router as teacher_student_add_router
-from .teacher_student_delete import router as teacher_student_delete_router
-from .teacher_student_list import router as teacher_student_list_router
 
 teacher_routers = [
-    teacher_lesson_add_router,
     teacher_slot_add_router,
-    teacher_student_add_router,
-    teacher_student_delete_router,
-    teacher_student_list_router,
 ]
+
+teacher_routers.extend(lesson_routers)
+teacher_routers.extend(student_routers)
---
+++ app/handlers/callbacks/teacher/lesson/__init__.py
@@ -0,0 +1,5 @@
+from .teacher_lesson_add import router as teacher_lesson_add_router
+
+lesson_routers = [
+    teacher_lesson_add_router,
+]
