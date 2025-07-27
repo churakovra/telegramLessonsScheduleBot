bcb11cf32babb20847e37d9851c6e39d59a073b5
churakovra <churakov.main@gmail.com>
2025-07-27 22:53:27 +0300

    feature(lessons): create teacher lesson delete router

--- app/handlers/callbacks/teacher/lesson/__init__.py
+++ app/handlers/callbacks/teacher/lesson/__init__.py
@@ -1,5 +1,7 @@
 from .teacher_lesson_add import router as teacher_lesson_add_router
+from .teacher_lesson_delete import router as teacher_lesson_delete_router
 
 lesson_routers = [
     teacher_lesson_add_router,
+    teacher_lesson_delete_router,
 ]
