1861bb591a69fa29375f95d9c18848611492b6a7
churakovra <churakov.main@gmail.com>
2025-07-28 22:29:11 +0300

    feature(lessons): fix routers

--- app/handlers/callbacks/teacher/__init__.py
+++ app/handlers/callbacks/teacher/__init__.py
@@ -1,15 +1,9 @@
 from .lesson import lesson_routers
-from .send_slots import router as send_slots_router
-
-from .slots_correct import router as slots_correct_router
+from .slot import slot_routers
 from .student import student_routers
 
-
-from .teacher_slot_add import router as teacher_slot_add_router
-
-teacher_routers = [
-    teacher_slot_add_router,
-]
+teacher_routers = []
 
 teacher_routers.extend(lesson_routers)
+teacher_routers.extend(slot_routers)
 teacher_routers.extend(student_routers)
---
+++ app/handlers/callbacks/teacher/slot/__init__.py
@@ -0,0 +1,11 @@
+from .send_slots import router as send_slots_router
+from .slots_correct import router as slots_correct_router
+from .slots_incorrect import router as slots_incorrect_router
+from .teacher_slot_add import router as teacher_slot_add_router
+
+slot_routers = [
+    send_slots_router,
+    slots_correct_router,
+    slots_incorrect_router,
+    teacher_slot_add_router,
+]
