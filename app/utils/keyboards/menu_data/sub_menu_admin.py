ec3531825ce6ec6c44d1bfa922d89cadea921a5c
churakovra <churakov.main@gmail.com>
2025-08-17 01:10:47 +0300

    fix(bot-0.0.5): change SubMenuData type from tuple to list for better typechecking

--- app/utils/keyboards/menu_data/sub_menu_admin.py
+++ app/utils/keyboards/menu_data/sub_menu_admin.py
@@ -4,8 +4,12 @@ from app.schemas.button_dto import ButtonDTO
 from app.utils.enums.menu_type import MenuType
 from app.utils.keyboards.callback_factories.sub_menu import SubMenuCallback
 
+
 @dataclass
 class SubMenuDataAdmin:
-    admin_temp = (
-        ButtonDTO(text="Пока командой", callback_data=SubMenuCallback(menu_type=MenuType.ADMIN_TEMP)),
-    )  
+    admin_temp = [
+        ButtonDTO(
+            text="Пока командой",
+            callback_data=SubMenuCallback(menu_type=MenuType.ADMIN_TEMP),
+        ),
+    ]
