977a8778472af60a4d427dc3a4c739f5b400abbd
churakovra <churakov.main@gmail.com>
2025-08-17 23:16:05 +0300

    refactor(bot-0.0.5): fix DB_PORT type & DBSessionMiddleware __call__ prop types

--- app/middlewares/db_session.py
+++ app/middlewares/db_session.py
@@ -1,7 +1,8 @@
-from typing import Callable, Dict, Any, Awaitable
+from typing import Any
+from collections.abc import Callable, Awaitable
 
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import TelegramObject
 
 from app.db.database import async_session_factory
 
@@ -9,9 +10,9 @@ from app.db.database import async_session_factory
 class DBSessionMiddleware(BaseMiddleware):
     async def __call__(
             self,
-            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message,
-            data: Dict[str, Any]
+            handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: dict[str, Any]
     ) -> Any:
         async with async_session_factory() as session:
             data["session"] = session
