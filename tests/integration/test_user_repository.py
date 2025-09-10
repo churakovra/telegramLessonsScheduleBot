1577a5e6a50f64533635a2cbf0dc484e13f6fbac
churakovra <churakov.main@gmail.com>
2025-09-10 23:12:10 +0300

    wip(bot-0.0.5): add filling db with user data fixture

--- tests/integration/test_user_repository.py
+++ tests/integration/test_user_repository.py
@@ -23,6 +23,20 @@ class Base:
     def setup_repo(self, setup_session):
         self.repo = UserRepository(setup_session)
 
+    @pytest.fixture
+    async def insert_user(self):
+        async def _wrap(cnt: int = 1):
+            for i in range(cnt):
+                user = UserDTO.new_dto(
+                    username=f"username_{i}",
+                    firstname=f"firstname_{i}",
+                    lastname=f"lastname_{i}",
+                    role=UserRoles.STUDENT,
+                    chat_id=i
+                )
+
+                await self.repo.add_user(user)
+
 
 class TestAddUser(Base):
     async def test_add_user_success(self, user_data):
