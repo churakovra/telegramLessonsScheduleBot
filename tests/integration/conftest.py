2c8b88306be71f62586fb85d1410148ba6b90c16
churakovra <churakov.main@gmail.com>
2025-09-12 23:33:31 +0300

    test(bot-0.0.5): add test_get_user_returns_userdto; mv user fixtures in integration/conftest.py; create unified fixture new_user

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,6 +4,9 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
 
 from app.db.database import url
+from app.repositories.user_repository import UserRepository
+from app.schemas.user_dto import UserDTO
+from app.utils.enums.bot_values import UserRoles
 
 
 @pytest.fixture(scope="session")
@@ -26,3 +29,40 @@ async def clear_tables(setup_engine: AsyncEngine):
     yield
     async with setup_engine.begin() as conn:
         await conn.execute(text(query))
+
+
+@pytest.fixture
+def new_user():
+    def wrap(
+        *,
+        username: str,
+        role: UserRoles = UserRoles.STUDENT,
+        cnt: int = 1,
+    ):
+        return UserDTO.new_dto(
+            username=username,
+            firstname=f"firstname_{cnt}",
+            lastname=f"lastname_{cnt}",
+            role=role,
+            chat_id=cnt,
+        )
+
+    return wrap
+
+
+@pytest_asyncio.fixture(loop_scope="session")
+async def insert_user(
+    request,
+    setup_session,
+    new_user,
+):
+    params = request.param
+    cnt = params.get("cnt", 1)
+    role = params.get("role", UserRoles.STUDENT)
+    username = params.get("username", None)
+
+    repo = UserRepository(setup_session)
+
+    for i in range(cnt):
+        user = new_user(username=username, role=role, cnt=i)
+        await repo.add_user(user)
--- tests/integration/test_user_repository.py
+++ tests/integration/test_user_repository.py
@@ -1,48 +1,11 @@
 import pytest
-import pytest_asyncio
 
 from app.db.orm.slot import Slot
 from app.db.orm.user import User
 from app.repositories.user_repository import UserRepository
 from app.schemas.user_dto import UserDTO
-from app.utils.enums.bot_values import UserRoles
 
 pytestmark = pytest.mark.asyncio(loop_scope="session")
-
-
-@pytest.fixture
-def user_data() -> UserDTO:
-    return UserDTO.new_dto(
-        username="test",
-        firstname="test",
-        lastname=None,
-        role=UserRoles.STUDENT,
-        chat_id=12345,
-    )
-
-
-@pytest_asyncio.fixture(loop_scope="session")
-async def insert_user(
-    request,
-    setup_session,
-):
-    params = request.param
-    cnt = params.get("cnt", 1)
-    role = params.get("role", UserRoles.STUDENT)
-    username = params.get("username", None)
-
-    repo = UserRepository(setup_session)
-
-    for i in range(cnt):
-        user = UserDTO.new_dto(
-            username=username if username else f"username_{i}",
-            firstname=f"firstname_{i}",
-            lastname=f"lastname_{i}",
-            role=role,
-            chat_id=i,
-        )
-
-        await repo.add_user(user)
 
 
 class Base:
@@ -52,9 +15,10 @@ class Base:
 
 
 class TestAddUser(Base):
-    async def test_add_user_success(self, user_data):
-        await self.repo.add_user(user_data)
-        user = await self.repo.get_user("test")
+    async def test_add_user_success(self, new_user):
+        await self.repo.add_user(new_user(username="test_username"))
+        user = await self.repo.get_user("test_username")
+
         assert user
 
 
@@ -72,3 +36,11 @@ class TestGetUser(Base):
         user = await self.repo.get_user("unknown-username")
 
         assert not user
+
+    @pytest.mark.parametrize(
+        "insert_user", [{"username": "test_username"}], indirect=True
+    )
+    async def test_get_user_returns_userdto(self, insert_user):
+        user = await self.repo.get_user("test_username")
+
+        assert isinstance(user, UserDTO)
