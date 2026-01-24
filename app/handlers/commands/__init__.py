730d29b6fa18fbd826026bde7750277929586e3c
churakovra <churakov.main@gmail.com>
2026-01-24 23:21:46 +0300

    feature: add cancel command handler

--- app/handlers/commands/__init__.py
+++ app/handlers/commands/__init__.py
@@ -1,8 +1,10 @@
+from .cancel import router as cancel_router
 from .make_teacher import router as make_teacher_router
 from .menu import router as menu_router
 from .start import router as start_router
 
 command_routers = [
+    cancel_router,
     make_teacher_router,
     menu_router,
     start_router,
---
+++ app/handlers/commands/cancel.py
@@ -0,0 +1,25 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from app.handlers.commands.menu import markup_type_by_role
+from app.keyboard.builder import MarkupBuilder
+from app.services.user_service import UserService
+from app.utils import message_template
+
+
+router = Router()
+
+
+@router.message(Command("cancel"))
+async def cancel(message: Message, session: AsyncSession, state: FSMContext) -> None:
+    user_service = UserService(session)
+    username = message.from_user.username
+    user = await user_service.get_user(username)
+    markup = MarkupBuilder.build(markup_type_by_role[user.role])
+    reply_message = message_template.main_menu_message(markup)
+    await state.clear()
+    await message.answer(**reply_message)
